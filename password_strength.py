66a961aab84ba808adef09a07772adf48dad7973
Nicolay Poymanov <n.poymanov@gmail.com>
2018-08-27 10:42:38 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -2,15 +2,23 @@ import re
 import sys
 import string
 import os
+import argparse
 from getpass import getpass
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--blacklist', help='Path to blacklist file')
+    return parser.parse_args()
+
+
 def check_password_inclusion_values(password):
-    rules = [r'[a-z]+', r'[A-Z]+', r'{}'.format(re.escape(string.punctuation))]
+    patterns = [r'[a-z]+', r'[A-Z]+',
+                r'{}'.format(re.escape(string.punctuation))]
     rating = 0
 
-    for rule in rules:
-        if re.search(rule, password):
+    for pattern in patterns:
+        if re.search(pattern, password):
             rating += 1
 
     return rating
@@ -26,30 +34,31 @@ def check_password_length(password):
     return rating
 
 
-def check_password_in_blacklist(password):
-    rating = 2
-
-    blacklist_path = 'blacklist.txt'
-
-    if not os.path.exists(blacklist_path):
-        print("Can't find blacklist file. This check will be skipped")
-        rating = 0
-    else:
-        with open(blacklist_path) as blacklist_file:
+def load_blacklist(filepath):
+    try:
+        with open(filepath) as blacklist_file:
             blacklist_list = blacklist_file.read().splitlines()
 
-        if password in blacklist_list:
-            rating = 0
+        return blacklist_list
+    except (TypeError, IOError):
+        return None
+
+
+def check_password_in_blacklist(password, blacklist_list):
+    if password not in blacklist_list:
+        rating = 2
+    else:
+        rating = 0
 
     return rating
 
 
 def check_password_prohibited_values(password):
     rating = 0
-    rules = [r'\d{2,4}[\.-]\d{2}[\.-]\d{2,4}', r'\+?\d+[-(\s]\d+[-)\s].+']
+    patterns = [r'\d{2,4}[\.-]\d{2}[\.-]\d{2,4}', r'\+?\d+[-(\s]\d+[-)\s].+']
 
-    for rule in rules:
-        if not re.search(rule, password):
+    for pattern in patterns:
+        if not re.search(pattern, password):
             rating += 1
 
     return rating
@@ -64,24 +73,33 @@ def get_rating_description(rating):
         return 'Strong'
 
 
-def get_password_strength(password):
+def get_password_strength(password, blacklist_list):
     rating = []
     rating.append(check_password_inclusion_values(password))
     rating.append(check_password_length(password))
-    rating.append(check_password_in_blacklist(password))
     rating.append(check_password_prohibited_values(password))
+
+    if blacklist_list:
+        rating.append(check_password_in_blacklist(password, blacklist_list))
 
     return sum(rating)
 
 
 if __name__ == '__main__':
+    args = parse_args()
+
     password = getpass(
                prompt='Enter the password to calculate its complexity: ')
 
     if not password:
         sys.exit('You entered empty password')
 
-    rating = get_password_strength(password)
+    blacklist_list = load_blacklist(args.blacklist)
+
+    if not blacklist_list:
+        print("Failed to read blacklist file. This check will be skipped")
+
+    rating = get_password_strength(password, blacklist_list)
     rating_description = get_rating_description(rating)
 
     print('Your password rating is {} ({})'.format(rating, rating_description))
